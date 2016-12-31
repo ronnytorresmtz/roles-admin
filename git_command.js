111b5d7247d12d873053aff5d76019555761dca1
ronny torres <ronnytorresmtz@gmail.com>
2016-12-31 12:07:31 -0600

    Git Command help File

---
+++ git_command.js
@@ -0,0 +1,11 @@
+
+
+Git an GitHub Commands
+======================
+
+//create branch
+git branch {Branch Name}
+
+//Delete Local and Remote Branch
+git branch -D {Branch Name}
+git push origin --delete {Branch Name}
