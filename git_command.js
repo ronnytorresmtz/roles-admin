91d6de588b0fbb8707250670a9ec6b4d8676f11e
ronny torres <ronnytorresmtz@gmail.com>
2017-01-01 14:00:34 -0600

    updata git_command files with merge instrucctions

--- git_command.js
+++ git_command.js
@@ -1,11 +1,16 @@
-
-
 Git an GitHub Commands
 ======================
 
+BRANCHES
 //create branch
 git branch {Branch Name}
 
 //Delete Local and Remote Branch
 git branch -D {Branch Name}
 git push origin --delete {Branch Name}
+
+MERGE
+//Merge branches with master
+git checkout master
+gti merge {Branch Name}
+git push origin master
