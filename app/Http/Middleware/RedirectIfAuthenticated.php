302bb7d3ab2aa9ddba0e352d0a61471e0aba125d
ronny torres <ronnytorresmtz@gmail.com>
2017-01-05 22:05:03 -0600

    Commentted the New RedirectReponse to go /home

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -33,12 +33,17 @@ class RedirectIfAuthenticated {
 	 */
 	public function handle($request, Closure $next)
 	{
-		if ($this->auth->check())
-		{
-			return new RedirectResponse(url('/home'));
-		}
+		
+		//return view('vueroute');
+		
+		// if ($this->auth->check())
+		// {
+		// 	//return new RedirectResponse(url('/home'));
+			
+		// }
 
 		return $next($request);
+		
 	}
 
 }
