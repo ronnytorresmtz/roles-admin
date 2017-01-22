cf86621536a2a8d20d3b1619fe079b951e2ecec9
ronny torres <ronnytorresmtz@gmail.com>
2017-01-21 18:41:28 -0600

    add routes to the guest middleware

--- app/Http/Routes/Login/login.php
+++ app/Http/Routes/Login/login.php
@@ -15,6 +15,31 @@ Route::group(array('middleware' => 'guest'), function(){
 		'uses'			=> 'LoginController@getLogIn'
 	));
 
+	Route::post('login/sendYourPassword', array(
+
+	'as' 			=> 'login.sendYourPassword',
+	'uses'			=> 'LoginController@postSendYourPassword'
+	));
+
+	Route::post('login/resetYourPassword', array(
+
+		'as'			=> 'login.resetYourPassword',
+		'uses'			=> 'LoginController@postResetYourPassword'
+	));
+
+	Route::get('login/userAuthenticated', array(
+
+		'as'			=> 'login.userAuthenticated',
+		'uses'			=> 'LoginController@getUserAuthenticated'
+	));
+
+	Route::get('login/tokenExist', array(
+
+		'as'			=> 'login.tokenExist',
+		'uses'			=> 'LoginController@getTokenExist'
+	));
+
+
 });
 
 
@@ -34,46 +59,6 @@ Route::post('login/logIn', array(
 	'uses'			=> 'LoginController@postLogIn'
 ));	
 
-
-// Route::get('login/forgotYourPassword', array(
-
-// 	'as'			=> 'login.forgotYourPassword',
-// 	'uses'			=> 'LoginController@getForgotYourPassword'
-// ));
-
-Route::post('login/sendYourPassword', array(
-
-	'as' 			=> 'login.sendYourPassword',
-	'uses'			=> 'LoginController@postSendYourPassword'
-));
-
-// Route::group(array('middleware' => 'token.verification'), function(){
-
-// 	Route::post('login/passwordReset/{token}', array(
-
-// 		'as' 			=> 'login.passwordReset',
-// 		'uses'			=> 'LoginController@getPasswordReset'
-// 	));
-	
-// });
-
-Route::post('login/resetYourPassword', array(
-
-	'as'			=> 'login.resetYourPassword',
-	'uses'			=> 'LoginController@postResetYourPassword'
-));
-
-Route::get('login/userAuthenticated', array(
-
-	'as'			=> 'login.userAuthenticated',
-	'uses'			=> 'LoginController@getUserAuthenticated'
-));
-
-Route::get('login/tokenExist', array(
-
-	'as'			=> 'login.tokenExist',
-	'uses'			=> 'LoginController@getTokenExist'
-));
 
 
 
