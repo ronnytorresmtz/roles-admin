b37a2a89f832e1b992f31b3040cdf4e2ddeafe43
ronny torres <ronnytorresmtz@gmail.com>
2017-02-20 21:47:51 -0600

    linux commands

--- linux_commands.js
+++ linux_commands.js
@@ -4,3 +4,25 @@ Linux Commands
 
 Change TimeZone
 - dpkg-reconfigure tzdata
+
+To know the server IP Address
+- ifconfig
+
+Disk Used and Avalilable Space
+-df --total
+
+Remove applications
+-sudo apt-get remove <application_name>
+
+Add a Super User
+-sudo adduser <usrname>
+-usermod -aG sudo <username>
+
+To swith the Super User
+-su <username>
+
+List the directory and files with all details
+-ls -la
+
+-List of Open Port
+netstat -an | grep "LISTEN "
