21bc4b23284a89db58bd3c24ed40365377a76039
Rolando Torres <ronnytorresmtz@gmail.com>
2017-02-25 15:03:13 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 # script/after_install.sh
 #!/bin/bash
-sudo cp -r ../public/ /var/www/html/roles-admin/public
+sudo cp -r ../public/ /var/www/html/roles-admin
 #sudo cp /var/www/html/roles-admin/public/index.bak /var/www/html/roles-admin/public.index.php
