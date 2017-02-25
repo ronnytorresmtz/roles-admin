610de02340ae30a88150ea606e3433e60de51beb
Rolando Torres <ronnytorresmtz@gmail.com>
2017-02-25 16:30:34 -0600

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,6 @@
 # script/before_install.sh
 #!/bin/bash
+#remove all directories and files in /var/repo
 sudo rm -r /var/repo
+#remove all directories and files in /var/www/html/roles-admin
 sudo rm -r /var/www/html/roles-admin
