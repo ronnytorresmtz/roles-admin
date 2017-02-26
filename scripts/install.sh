ba5a53d52635586480641816780589424ded10e3
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 18:26:46 -0600

    adjust after install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #sudo cp -r ../public/ /var/www/html/roles-admin
 sudo rm -f /var/www/html/roles-admin/public/index.php
-sudo cp /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
+sudo cp -f /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
 
 
 
