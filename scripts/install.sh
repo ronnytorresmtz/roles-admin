b893328c3cd4eb581496869ff9d314bcf57abfc1
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 18:03:51 -0600

    copy index.prod to index.php

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 # script/after_install.sh
 #!/bin/bash
 #sudo cp -r ../public/ /var/www/html/roles-admin
-#sudo cp /var/www/html/roles-admin/public/index.prod /var/www/html/roles-admin/public.index.php/
+sudo cp /var/www/html/roles-admin/public/index.prod.php /var/www/html/roles-admin/public/index.php
 
 
