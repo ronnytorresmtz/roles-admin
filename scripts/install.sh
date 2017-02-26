0c3e532b8a7ead63b2b1b03166d0a2ea6ef89cfd
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 18:25:03 -0600

    adjust after_install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 # script/after_install.sh
 #!/bin/bash
 #sudo cp -r ../public/ /var/www/html/roles-admin
-sudo rm /var/www/html/roles-admin/public/index.php
+sudo rm -f /var/www/html/roles-admin/public/index.php
 sudo cp /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
 
 
