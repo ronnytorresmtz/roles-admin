125b4cd17266e3555c656cb492b36b263544d828
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 20:15:22 -0600

    change arter_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,7 @@
 # script/after_install.sh
 #!/bin/bash
+
+#replace the laravel public/index.php with a index.php for production enviroment
 sudo cp -rf /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
 
 
