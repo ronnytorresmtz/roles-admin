0c28eeda19c92946c84d6344d7610dfd1a203c28
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 19:57:56 -0600

    change start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,6 @@
 # script/after_install.sh
 #!/bin/bash
+sudo rm -r /var/www/html/roles-admin/public/index.php
+sudo cp -rf /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
+
 sudo service apache2 restart
