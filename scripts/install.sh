598c1ecf6743b0f32359b1829a9542fae875ecc7
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 19:35:39 -0600

    change install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,7 @@
 # script/after_install.sh
 #!/bin/bash
 #sudo cp -r ../public/ /var/www/html/roles-admin
-sudo rm -f /var/www/html/roles-admin/public/index.php
-sudo cp -f /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
+sudo cp -rf /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
 
 
 
