317a9469ccff163542b7d38386433eb95e3a8061
ronny torres <ronnytorresmtz@gmail.com>
2017-02-25 18:14:23 -0600

    Change name to index_prod.php

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 # script/after_install.sh
 #!/bin/bash
 #sudo cp -r ../public/ /var/www/html/roles-admin
-sudo cp - f /var/www/html/roles-admin/public/index.prod.php /var/www/html/roles-admin/public/index.php
+sudo cp - f /var/www/html/roles-admin/public/index_prod.php /var/www/html/roles-admin/public/index.php
 
 
 
