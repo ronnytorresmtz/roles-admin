9aa7c06d06c888e1fc9deeac7d7841cb9ffe0dcf
ronny torres <ronnytorresmtz@gmail.com>
2017-03-02 23:10:48 -0600

    change path to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 // echo dirname(app_path() . '\Http\Routes');	
 // echo dirname(__DIR__ . '\Routes');
 // Get the split routes files that are in the app/Http/Routes 
-$allFiles = File::allFiles('var\www\html\roles-admin\MyCode\Routes');
+$allFiles = File::allFiles('var/www/html/roles-admin/MyCode/Routes');
 //$allFiles = File::allFiles(__DIR__ . '\Routes');
 
 foreach ($allFiles as $file) {
